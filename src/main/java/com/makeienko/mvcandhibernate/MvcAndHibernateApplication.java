a20c5b473b29edbc2eed18d6d77f81589a526aa8
Oleg Makeienko <molva83@gmail.com>
2024-01-30 13:53:03 +0100

    project structure is done

---
+++ src/main/java/com/makeienko/mvcandhibernate/MvcAndHibernateApplication.java
@@ -0,0 +1,13 @@
+package com.makeienko.mvcandhibernate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MvcAndHibernateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MvcAndHibernateApplication.class, args);
+    }
+
+}
